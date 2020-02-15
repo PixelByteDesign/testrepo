a85479dd800a3bf06883bb384264064c15144b11
PixelByteDesign <hugocorreagomez@hotmail.com>
2020-02-15 16:01:52 -0500

    Skeleton of the HTML5
    This is just the skeleton of the HTML5 for the app, just take care about it and don't be worry about
    
    See: #2

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Todo List</title>
+</head>
+<body>
+	
+	<h1>ToDo List</h1>
+	
+	<h3>To Do</h3>
+	
+	<ul>
+		<li>Attach some important files to the main one</li>
+		<li>Excecute the code in the main repository</li>
+		<li>Change the code for the updated one</li>
+	</ul>
+	
+	<h3>Done</h3>
+	
+	<ul>
+		<li>Excert the text</li>
+	</ul>
+	
+</body>
+</html>
